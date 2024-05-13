cba9dd7b02b87870e6e7dd56c018f35f98266b6a
Pabitra3 <pabitrasahoo3691@gmail.com>
2024-05-13 20:42:23 +0530

    Add new file

---
+++ Example_Questions/Question2.py
@@ -0,0 +1,8 @@
+#  Calculate the sum of two numbers entered by the user
+
+###################### Code #######################
+
+a = int(input("Enter the first number:\n"))
+b = int(input("Enter the second number:\n"))
+c = a+b
+print(f"The sum of two numbers is:{c}")
