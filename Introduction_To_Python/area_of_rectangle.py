4b105d60215bfe61e2897a168ee7fdd0068d424e
Pabitra3 <pabitrasahoo3691@gmail.com>
2024-05-14 17:48:20 +0530

    Modified file

--- Introduction_To_Python/area_of_rectangle.py
+++ Introduction_To_Python/area_of_rectangle.py
@@ -1,3 +1,6 @@
+# Write a Python program to calculate the area of a rectangle given its length and width
+
+
 # Ask the user to enter the length and width
 length = float(input("Enter the length of the rectangle: "))
 width = float(input("Enter the width of the rectangle: "))
